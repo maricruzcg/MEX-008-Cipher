f5ecb4d6eb85f8658bc9d3b2906817cd2c5427a6
maricruzcg <maricruz7@live.com.mx>
2019-06-19 18:29:10 -0500

    subiendo cifrado y descifrado, html y css

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,38 @@
 window.cipher = {
-  // ... 
+  encode : (offset , str) =>{ //Crear una función con dos parametros texto y desplazamiento
+  //Necesito conocer el valor ASCII de cada letra text = text.charCodeAt(4);
+    let encryptNumberAscci = '';
+    let encryptStr = '';
+    for(let i = 0; i < str.length; i++) { //Utilizo for para saber la posicion de cada una de las letras de la palabra
+      let numberAscii = str.charCodeAt(i); //definir variable para saber saber la posicion de cada letra
+      if(numberAscii >= 65 && numberAscii <= 90){ //Si la letra es mayuscula
+        encryptNumberAscci = (numberAscii - 65 + offset) % 26 + 65;
+      }else if (numberAscii >= 97 && numberAscii <= 122){ // si la letra es minuscula
+        encryptNumberAscci = (numberAscii - 97 + offset) % 26 + 97;
+      }else {
+        encryptNumberAscci = numberAscii; // Si la letra es un simbolo o numero
+      }
+      encryptStr += String.fromCharCode(encryptNumberAscci);
+    };
+    return encryptStr;
+  },
+
+  decode : (offset, str) => {
+  let decryptedNumberAscci = '';
+  let decryptedStr = '';
+
+  for(let i = 0; i < str.length; i++) {
+    let backAscii = str.charCodeAt(i);
+    if(backAscii >= 65 && backAscii <= 90){
+      decryptedNumberAscci = (backAscii - 65 - offset) % 26 + 65;
+    }else if (backAscii >= 97 && backAscii <= 120){
+      decryptedNumberAscci = (backAscii - 97 - offset) % 26 + 97;
+    }else {
+      decryptedNumberAscci = backAscii;
+    }
+    decryptedStr += String.fromCharCode(decryptedNumberAscci);
+
+  }
+  return decryptedStr;
+  }
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ const inputName = document.getElementById('input-name');
 const buttonSesion = document.getElementById('button-sesion');
 const welcome = document.getElementById('welcome');
 const description = document.getElementById('description');
-const cifrar = document.getElementById('cifrar');
+const code = document.getElementById('cifrar');
+const inputCode = document.getElementById('input-cifrar');
+const offsetSecurity1 = document.getElementById('number-security1');
+const buttonCode = document.getElementById('button-code');
+const resultCode = document.getElementById('result-code');
+const inputDecode = document.getElementById('input-descifrar');
+const offsetSecurity2 = document.getElementById('number-security2');
+const buttonDecode = document.getElementById('button-decode');
+const resultDecode = document.getElementById('result-decode');
 
 const getName = () => {
   description.classList.add('page-two');
-  cifrar.classList.remove('page-two');
+  code.classList.remove('page-two');
   welcome.innerHTML = inputName.value + '<br/>' + 'Bienvenido(a)';
 }
 
+const getText = () => {
+  resultCode.innerHTML = 'Tu información ha sido cifrada exitosamente' + '<br/>' + window.cipher.encode(parseInt(offsetSecurity1.value), inputCode.value);
+}
+
+const getTextDecode = () => {
+  resultDecode.innerHTML = 'Tu código ha sido descifrado exitosamente' + '<br/>' + window.cipher.decode(parseInt(offsetSecurity2.value), inputDecode.value);
+
+}
+
+buttonDecode.addEventListener('click',getTextDecode);
+buttonCode.addEventListener('click', getText);
 buttonSesion.addEventListener('click', getName);
